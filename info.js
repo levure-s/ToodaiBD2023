80221d93f3032209d0be904dfffba439732219c0
levure-s <levure.sauvage@gmail.com>
2022-08-01 01:21:05 +0900

    first_commit

---
+++ info.js
@@ -0,0 +1,34 @@
+const tweets = {
+  // ukyo: {
+  //   img: "bdms9.png",
+  //   url: "https://twitter.com/OuSkNySo_1116/status/1421485825913548800",
+  // },
+  tacchan: {
+    img: "tacchan.jpg",
+    url: "https://twitter.com/Uekid1/status/1553760874652770305",
+  },
+  chiya: {
+    img: "chiya.jpg",
+    url: "https://twitter.com/chiya__k2305/status/1553760797184393216",
+  },
+  // hatomo: {
+  //   img: "bdms4.png",
+  //   url: "https://twitter.com/hatomo2192/status/1421486560814403589",
+  // },
+  ryosuke: {
+    img: "ryosuke.jpg",
+    url: "https://twitter.com/R_atslh/status/1553757771283451904",
+  },
+  yuna: {
+    img: "yuna.jpg",
+    url: "https://twitter.com/KnwRwi/status/1553757386019852288",
+  },
+  murakami: {
+    img: "murakami.jpg",
+    url: "https://twitter.com/kaiyouseimei214/status/1553773282133831680",
+  },
+  ady: {
+    img: "ady.jpg",
+    url: "https://twitter.com/mABxYpubO4HVZCP/status/1553758872720265218",
+  },
+};

8d177b77f8a2bf0674d639da53e6a90f400706c9
levure-s <levure.sauvage@gmail.com>
2023-08-01 11:40:29 +0900

    add_ryosuke

--- info.js
+++ info.js
@@ -7,6 +7,10 @@ const tweets = {
     img: "norio.jpg",
     url: "https://twitter.com/blx3xld/status/1686030634467430400",
   },
+  ryosuke: {
+    img: "ryosuke.jpg",
+    url: "https://twitter.com/R_atslh/status/1686037411468578816",
+  },
   levu: {
     img: "levu.jpg",
     url: "https://twitter.com/8J6pxh2o0XB5lN9/status/1686036604450926593",

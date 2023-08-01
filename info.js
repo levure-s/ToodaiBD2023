2bab2788096a97e1f392f5786a8b85e49e656bfe
levure-s <levure.sauvage@gmail.com>
2023-08-01 10:23:51 +0900

    add_norio

--- info.js
+++ info.js
@@ -3,6 +3,10 @@ const tweets = {
     img: "tacchan.jpg",
     url: "https://twitter.com/T_sakura_0113/status/1686044660270235648",
   },
+  norio: {
+    img: "norio.jpg",
+    url: "https://twitter.com/blx3xld/status/1686030634467430400",
+  },
   levu: {
     img: "levu.jpg",
     url: "https://twitter.com/8J6pxh2o0XB5lN9/status/1686036604450926593",

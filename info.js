ea211966d7258fbbaedb36725954268e42c96ac4
levure-s <levure.sauvage@gmail.com>
2023-08-01 12:41:16 +0900

    add_haruki_ayaka_koto

--- info.js
+++ info.js
@@ -1,4 +1,12 @@
 const tweets = {
+  haruki: {
+    img: "haruki.jpg",
+    url: "https://twitter.com/haru4869_Babys/status/1686214739825934336",
+  },
+  ayuka: {
+    img: "ayuka.jpg",
+    url: "https://twitter.com/Ayukababys1/status/1686215362747170816",
+  },
   tacchan: {
     img: "tacchan.jpg",
     url: "https://twitter.com/T_sakura_0113/status/1686044660270235648",
@@ -14,6 +22,10 @@ const tweets = {
   shi: {
     img: "shi.jpg",
     url: "https://twitter.com/xUUHU0rwgUqqlOD/status/1686199495766474753",
+  },
+  koto: {
+    img: "koto.jpg",
+    url: "https://twitter.com/Babys_08/status/1686210080482705408",
   },
   levu: {
     img: "levu.jpg",
